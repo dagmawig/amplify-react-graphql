3e06eba09abcdb699540494af041486dbdd1cc2a
dagmawig <dgebreselasse@gmail.com>
2024-02-11 15:03:21 -0600

    second commit added simple button

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%'}}>
+      <button style={{backgroundColor: 'red', height: 100, borderRadius: 20}}>Press me but I do nothing!</button>
     </div>
   );
 }
